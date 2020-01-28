8d98d8d4f415e8c14c1a814f482464569a1cee97
João Portela <ei04034+github@gmail.com>
2020-01-28 11:10:50 +0000

    Migrate to dotnet core project format.
    re-created .sln and .csproj files using dotnet new.
    example and tests target .net core 3.1.
    library targets .net standard 2.0.
    removes unnecessary files like AssemblyInfo.cs.
    removes packages.config in favor of new format.

--- CircularBufferExample/Program.cs
+++ CircularBufferExample/Program.cs
@@ -1,35 +1,12 @@
-using System;
-using CircularBuffer;
+﻿using System;
 
 namespace CircularBufferExample
 {
-    static class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var buffer = new CircularBuffer<int>(5);
-
-            for (int i = 0; i < 5; i++)
-            {
-                buffer.PushBack(i);
-            }
-
-            for (int i = 0; i < buffer.Size; i++)
-            {
-                Console.WriteLine(buffer[i]);
-            }
-
-            Console.WriteLine("---");
-
-            for (int i = 0; i < 3; i++)
-            {
-                buffer.PushBack(10 + i);
-            }
-            
-            for (int i = 0; i < buffer.Size; i++)
-            {
-                Console.WriteLine(buffer[i]);
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
