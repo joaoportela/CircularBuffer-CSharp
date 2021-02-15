8c33a70ffa843a5a866869104dc3e892800f6620
João Portela <ei04034+github@gmail.com>
2021-02-15 23:44:13 +0000

    Add example code

--- CircularBufferExample/Program.cs
+++ CircularBufferExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CircularBuffer;
 
 namespace CircularBufferExample
 {
@@ -6,7 +7,42 @@ namespace CircularBufferExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CircularBuffer<int> buffer = new CircularBuffer<int>(5, new[] {0, 1, 2});
+            Console.WriteLine("Initial buffer {0,1,2}:");
+            PrintBuffer(buffer);
+
+
+            for (int i = 3; i < 7; i++)
+            {
+                buffer.PushBack(i);
+            }
+            Console.WriteLine("\nAfter adding a 7 elements to a 5 elements capacity buffer:");
+            PrintBuffer(buffer);
+
+
+            buffer.PopFront();
+            Console.WriteLine("\nbuffer.PopFront():");
+            PrintBuffer(buffer);
+
+
+            buffer.PopBack();
+            Console.WriteLine("\nbuffer.PopBack():");
+            PrintBuffer(buffer);
+
+            for (int i = 2; i >= 0; i--)
+            {
+                buffer.PushFront(i);
+            }
+            Console.WriteLine("\nbuffer.PushFront() {2,1,0} respectively:");
+            PrintBuffer(buffer);
+        }
+
+        private static void PrintBuffer(CircularBuffer<int> buffer)
+        {
+            for (int i = 0; i < buffer.Size; i++)
+            {
+                Console.WriteLine($"buffer[{i}] = {buffer[i]}");
+            }
         }
     }
 }
